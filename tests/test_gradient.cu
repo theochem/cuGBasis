008482c431d22b5798c5db96a6d81d46c50818c6
Ali-Tehrani <alirezatehrani24@gmail.com>
2024-01-24 14:30:20 -0500

    Add test for gradient in row-order

--- tests/test_gradient.cu
+++ tests/test_gradient.cu
@@ -187,3 +187,86 @@ assert np.all(error < 1e-10), "Gradient on electron density on GPU doesn't match
   } // Need this so that the python object doesn't outline the interpretor when we close it up.
   //py::finalize_interpreter(); // Close up the python interpretor for this test.
 }
+
+
+
+TEST_CASE( "Test Gradient of Electron Density Against gbasis (Col Order)", "[evaluate_electron_density_gradient_col]" ) {
+  //py::initialize_interpreter();  // Open up the python interpretor for this test.
+  {  // Need this so that the python object doesn't outline the interpretor.
+    // Get the IOdata object from the fchk file.
+    std::string fchk_file = GENERATE(
+        "./tests/data/atom_01_H_N01_M2_ub3lyp_ccpvtz_g09.fchk",
+        "./tests/data/atom_he.fchk",
+        "./tests/data/atom_be.fchk",
+        "./tests/data/atom_be_f_pure_orbital.fchk",
+        "./tests/data/atom_be_f_cartesian_orbital.fchk",
+        "./tests/data/atom_kr.fchk",
+        "./tests/data/atom_o.fchk",
+        "./tests/data/atom_c_g_pure_orbital.fchk",
+        "./tests/data/atom_mg.fchk",
+        "./tests/data/E948_rwB97XD_def2SVP.fchk",
+        "./tests/data/test.fchk",
+        "./tests/data/test2.fchk",
+        "./tests/data/atom_08_O_N08_M3_ub3lyp_ccpvtz_g09.fchk",
+        "./tests/data/atom_08_O_N09_M2_ub3lyp_ccpvtz_g09.fchk",
+        "./tests/data/4141_q000_m01_k00_force_uhf_ccpvtz.fchk",
+        "./tests/data/h2o.fchk",
+        "./tests/data/ch4.fchk",
+        "./tests/data/qm9_000092_HF_cc-pVDZ.fchk",
+        "./tests/data/qm9_000104_PBE1PBE_pcS-3.fchk"
+    );
+    std::cout << "FCHK FILE %s \n" << fchk_file << std::endl;
+    gbasis::IOData iodata = gbasis::get_molecular_basis_from_fchk(fchk_file);
+
+    // Gemerate random grid.
+    int numb_pts = 700000;
+    std::vector<double> points(3 * numb_pts);
+    std::random_device rnd_device;
+    std::mt19937  merseene_engine {rnd_device()};
+    std::uniform_real_distribution<double> dist {-5, 5};
+    auto gen = [&dist, &merseene_engine](){return dist(merseene_engine);};
+    std::generate(points.begin(), points.end(), gen);
+
+    // Calculate Gradient
+    printf("Calculate Gradient \n");
+    gbasis::add_mol_basis_to_constant_memory_array(iodata.GetOrbitalBasis(), false, false);
+    std::vector<double> gradient_result = gbasis::evaluate_electron_density_gradient(iodata, points.data(), numb_pts,
+                                                                                     false);
+    printf("Done Gradient \n");
+
+    // COnvert them (with copy) to python objects so that they can be transfered.
+    pybind11::array_t<double, pybind11::array::c_style | pybind11::array::forcecast>
+        py_result = gbasis::as_pyarray_from_vector(gradient_result);
+    pybind11::array_t<double, pybind11::array::c_style | pybind11::array::forcecast>
+        py_points = gbasis::as_pyarray_from_vector(points);
+
+    auto locals = py::dict("points"_a = py_points,
+                           "true_result"_a = py_result,
+                           "fchk_path"_a = fchk_file,
+                           "numb_pts"_a = numb_pts);
+    py::exec(R"(
+import numpy as np
+from gbasis.evals.density import evaluate_density_gradient, evaluate_density
+from iodata import load_one
+from gbasis.wrappers import from_iodata
+
+true_result = true_result.reshape((numb_pts, 3), order="F")  # Row-major order
+
+iodata = load_one(fchk_path)
+basis, type = from_iodata(iodata)
+rdm = (iodata.mo.coeffs * iodata.mo.occs).dot(iodata.mo.coeffs.T)
+points = points.reshape((numb_pts, 3), order="F")
+points = np.array(points, dtype=np.float64)
+
+indices_to_compute = np.random.choice(np.arange(len(points)), size=10000)
+true_result = true_result[indices_to_compute, :]
+points = points[indices_to_compute, :]
+
+gradient = evaluate_density_gradient(rdm, basis, points, coord_type=type)
+error = np.abs(gradient - true_result)
+print("Max, Mean, STD , Min error ", np.max(error), np.mean(error), np.std(error), np.min(error))
+assert np.all(error < 1e-10), "Gradient on electron density on GPU doesn't match gbasis."
+    )", py::globals(), locals);
+  } // Need this so that the python object doesn't outline the interpretor when we close it up.
+  //py::finalize_interpreter(); // Close up the python interpretor for this test.
+}
