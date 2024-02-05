b5a864b26d9e0f9d7380d19c97a6f72b7bc6e326
Ali-Tehrani <alirezatehrani24@gmail.com>
2024-02-05 12:57:29 -0500

    Clean up eletron density tests and test high pts
    - See if memory management works correctly

--- tests/test_electron_density.cu
+++ tests/test_electron_density.cu
@@ -13,8 +13,7 @@ namespace py = pybind11;
 using namespace py::literals;
 
 
-TEST_CASE( "Test Electron Density Against chemtools", "[evaluate_electron_density_on_cubic]" ) {
-  //py::initialize_interpreter();  // Open up the python interpretor for this test.
+TEST_CASE( "Test Electron Density Against Gbasis", "[evaluate_electron_density_on_cubic]" ) {
   {  // Need this so that the python object doesn't outline the interpretor.
   // Evaluate the electron density of this example.
   std::string fchk_file = GENERATE(
@@ -32,27 +31,23 @@ TEST_CASE( "Test Electron Density Against chemtools", "[evaluate_electron_densit
       "./tests/data/test2.fchk",
       "./tests/data/atom_08_O_N08_M3_ub3lyp_ccpvtz_g09.fchk",
       "./tests/data/atom_08_O_N09_M2_ub3lyp_ccpvtz_g09.fchk",
-      "./tests/data/4141_q000_m01_k00_force_uhf_ccpvtz.fchk",
       "./tests/data/h2o.fchk",
       "./tests/data/ch4.fchk",
       "./tests/data/qm9_000092_HF_cc-pVDZ.fchk",
       "./tests/data/qm9_000104_PBE1PBE_pcS-3.fchk"
     );
-  printf("IODATA OB %s \n", fchk_file.c_str());
+  printf("Test: %s \n", fchk_file.c_str());
   chemtools::IOData iodata = chemtools::get_molecular_basis_from_fchk(fchk_file);
   chemtools::UniformGrid grid =
       chemtools::get_grid_from_coordinates_charges(iodata.GetCoordAtoms(), iodata.GetCharges(), iodata.GetNatoms(), 0.0 );
-  printf("DOne grid \n");
   double3 l_bnd = {grid.l_bnd[0], grid.l_bnd[1], grid.l_bnd[2]};
   std::array<int, 3> shape_arr = grid.calculate_shape({0.9, 0.9, 0.9});
   std::array<double, 9> axes_spacing = grid.multiply_axes_by_spacing(0.9, 0.9, 0.9);
   int3 shape = {shape_arr[0], shape_arr[1], shape_arr[2]};
 
   // Evaluate electron density on the cube
-  printf("Electron density \n");
   chemtools::add_mol_basis_to_constant_memory_array(iodata.GetOrbitalBasis(), false, false);
   std::vector<double> result = chemtools::evaluate_electron_density_on_cubic(iodata, l_bnd, &axes_spacing[0], shape, false);
-  printf("DOne \n");
 
   //Transfer result to pybind11 without copying
   pybind11::array_t<double, pybind11::array::c_style | pybind11::array::forcecast> py_result = chemtools::as_pyarray_from_vector(result);
@@ -82,8 +77,9 @@ grid = l_bnd + random_indices.dot(axes)
 
 density = evaluate_density(rdm, basis, grid, coord_type=type)
 
-result = np.all(np.abs(density - true_result[global_index]) < 1e-8)
-print(np.abs(density - true_result[global_index]))
+err = np.abs(density - true_result[global_index])
+result = np.all(err < 1e-8)
+print(f"Max Error {np.max(err)}     Mean Err {np.mean(err)}    Std Err {np.std(err)}")
 assert result, "Electron density on GPU doesn't match gbasis."
     )", py::globals(), locals);
 
@@ -94,10 +90,8 @@ assert result, "Electron density on GPU doesn't match gbasis."
   //py::finalize_interpreter(); // Close up the python interpretor for this test.
 }
 
-
-
-TEST_CASE( "Test Electron Density Against gbasis on random grid", "[evaluate_electron_density_on_any_grid]" ) {
-  //py::initialize_interpreter();  // Open up the python interpretor for this test.
+TEST_CASE( "Test No Errors When Computing Electron Density At high points", "[evaluate_electron_density_many_pts]" ) {
+  // Goal of this test is to make sure the memory management is correct
   {  // Need this so that the python object doesn't outline the interpretor.
     // Evaluate the electron density of this example.
     std::string fchk_file = GENERATE(
@@ -115,17 +109,63 @@ TEST_CASE( "Test Electron Density Against gbasis on random grid", "[evaluate_ele
         "./tests/data/test2.fchk",
         "./tests/data/atom_08_O_N08_M3_ub3lyp_ccpvtz_g09.fchk",
         "./tests/data/atom_08_O_N09_M2_ub3lyp_ccpvtz_g09.fchk",
-        "./tests/data/4141_q000_m01_k00_force_uhf_ccpvtz.fchk",
         "./tests/data/h2o.fchk",
         "./tests/data/ch4.fchk",
         "./tests/data/qm9_000092_HF_cc-pVDZ.fchk",
         "./tests/data/qm9_000104_PBE1PBE_pcS-3.fchk"
     );
-    printf("IODATA OB %s \n", fchk_file.c_str());
+    printf("Test: %s \n", fchk_file.c_str());
     chemtools::IOData iodata = chemtools::get_molecular_basis_from_fchk(fchk_file);
 
     // Gemerate random grid.
-    int numb_pts = 10000;
+    int numb_pts = 10000000; // 10 million points
+    std::vector<double> points(3 * numb_pts);
+    std::random_device rnd_device;
+    std::mt19937  merseene_engine {rnd_device()};
+    std::uniform_real_distribution<double> dist {-5, 5};
+    auto gen = [&dist, &merseene_engine](){return dist(merseene_engine);};
+    std::generate(points.begin(), points.end(), gen);
+
+    // Evaluate electron density on the cube
+    chemtools::add_mol_basis_to_constant_memory_array(iodata.GetOrbitalBasis(), false, false);
+    std::vector<double> result = chemtools::evaluate_electron_density_on_any_grid(iodata, points.data(), numb_pts);
+
+    REQUIRE(cudaPeekAtLastError() == cudaSuccess);
+
+  } // Need this so that the python object doesn't outline the interpretor when we close it up.
+}
+
+
+
+TEST_CASE( "Test Electron Density Against gbasis on random grid", "[evaluate_electron_density_on_any_grid]" ) {
+  {  // Need this so that the python object doesn't outline the interpretor.
+    // Evaluate the electron density of this example.
+    std::string fchk_file = GENERATE(
+        "./tests/data/atom_01_H_N01_M2_ub3lyp_ccpvtz_g09.fchk",
+        "./tests/data/atom_he.fchk",
+        "./tests/data/atom_be.fchk",
+        "./tests/data/atom_be_f_pure_orbital.fchk",
+        "./tests/data/atom_be_f_cartesian_orbital.fchk",
+        "./tests/data/atom_kr.fchk",
+        "./tests/data/atom_o.fchk",
+        "./tests/data/atom_c_g_pure_orbital.fchk",
+        "./tests/data/atom_mg.fchk",
+        "./tests/data/E948_rwB97XD_def2SVP.fchk",
+        "./tests/data/test.fchk",
+        "./tests/data/test2.fchk",
+        "./tests/data/atom_08_O_N08_M3_ub3lyp_ccpvtz_g09.fchk",
+        "./tests/data/atom_08_O_N09_M2_ub3lyp_ccpvtz_g09.fchk",
+//        "./tests/data/4141_q000_m01_k00_force_uhf_ccpvtz.fchk",
+        "./tests/data/h2o.fchk",
+        "./tests/data/ch4.fchk",
+        "./tests/data/qm9_000092_HF_cc-pVDZ.fchk",
+        "./tests/data/qm9_000104_PBE1PBE_pcS-3.fchk"
+    );
+    printf("Test: %s \n", fchk_file.c_str());
+    chemtools::IOData iodata = chemtools::get_molecular_basis_from_fchk(fchk_file);
+
+    // Gemerate random grid.
+    int numb_pts = 10000000;
     std::vector<double> points(3 * numb_pts);
     std::random_device rnd_device;
     std::mt19937  merseene_engine {rnd_device()};
@@ -135,10 +175,8 @@ TEST_CASE( "Test Electron Density Against gbasis on random grid", "[evaluate_ele
 
 
     // Evaluate electron density on the cube
-    printf("Electron density \n");
-    chemtools::add_mol_basis_to_constant_memory_array(iodata.GetOrbitalBasis(), false, true);
+    chemtools::add_mol_basis_to_constant_memory_array(iodata.GetOrbitalBasis(), false, false);
     std::vector<double> result = chemtools::evaluate_electron_density_on_any_grid(iodata, points.data(), numb_pts);
-    printf("DOne \n");
 
     //Transfer result to pybind11 without copying
     pybind11::array_t<double, pybind11::array::c_style | pybind11::array::forcecast> py_result =
@@ -161,10 +199,13 @@ rdm = (iodata.mo.coeffs * iodata.mo.occs).dot(iodata.mo.coeffs.T)
 points = points.reshape((numb_pts, 3), order="F")
 points = np.array(points, dtype=np.float64)
 
-density = evaluate_density(rdm, basis, points, coord_type=type)
+random_indices = np.unique(np.random.randint(0, len(points), 50000))
+points = points[random_indices, :]
 
-result = np.all(np.abs(density - true_result) < 1e-8)
-print(np.abs(density - true_result))
+density = evaluate_density(rdm, basis, points, coord_type=type)
+err = np.abs(density - true_result[random_indices])
+result = np.all(err < 1e-8)
+print(f"Max Error {np.max(err)}     Mean Err {np.mean(err)}    Std Err {np.std(err)}")
 assert result, "Electron density on GPU doesn't match gbasis."
     )", py::globals(), locals);
 
@@ -172,5 +213,4 @@ assert result, "Electron density on GPU doesn't match gbasis."
       REQUIRE(true);
     }
   } // Need this so that the python object doesn't outline the interpretor when we close it up.
-  //py::finalize_interpreter(); // Close up the python interpretor for this test.
 }
