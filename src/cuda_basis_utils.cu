727dec28a7d33333b08f796729220415b528c577
Ali-Tehrani <alirezatehrani24@gmail.com>
2024-02-14 11:44:04 -0500

    Remove all pows in basis_utils
    - For effiency

--- src/cuda_basis_utils.cu
+++ src/cuda_basis_utils.cu
@@ -5,21 +5,26 @@
 
 /// Compute the normalization constant of a single primitive Cartesian Gaussian, S-type only.
 __device__ double chemtools::normalization_primitive_s(double alpha) {
-  return pow(2.0 * alpha / CUDART_PI_D, 3. / 4.);
+  double r = sqrt(2.0 * alpha / CUDART_PI_D);
+  return r * sqrt(r);
 }
 
 /// Compute the normalization constant of a single primitive Cartesian Gaussian, P-type only
 __device__ double chemtools::normalization_primitive_p(double alpha) {
-  return sqrt(pow(2.0 * alpha / CUDART_PI_D, 3. / 2.) * 4 * alpha);
+  double x = 2.0 * alpha / CUDART_PI_D;
+  return sqrt(x * sqrt(x) * 4.0 * alpha);
 }
 
 /// Compute the normalization constant of a single primitive Cartesian Gaussian, D-type only
 __device__ double chemtools::normalization_primitive_d(double alpha, int nx, int ny, int nz) {
   // (nx, ny, nz) are the angular components and sum to 2
   if (((nx == 1) & (ny == 1)) | ((nx == 1) & (nz == 1)) | ((ny == 1) & (nz == 1))) {
-    return pow(2.0 * alpha / CUDART_PI_D, 3. / 4.) * 4 * alpha;
+    double r = sqrt(2.0 * alpha / CUDART_PI_D);
+    return r * sqrt(r) * 4.0 * alpha;
   } else {
-    return sqrt(pow(2.0 * alpha / CUDART_PI_D, 3.0 / 2.0) * pow(4 * alpha, 2) / 3);
+    double x = 2.0 * alpha / CUDART_PI_D;
+    double a = 4.0 * alpha;
+    return sqrt(x * sqrt(x) * a * a / 3);
   }
 }
 
@@ -47,7 +52,10 @@ __device__ double chemtools::normalization_primitive_g(double alpha, int nx, int
   bool cond1 = ((nx == 4) & (ny == 0) & (nz == 0)) |
       ((nx == 0) & (ny == 4) & (nz == 0)) |
       ((nx == 0) & (ny == 0) & (nz == 4));
-  double num = pow(2.0 * alpha / CUDART_PI_D, 3.0 / 2.0) * pow(4 * alpha, 4);
+  double x = 2.0 * alpha / CUDART_PI_D;
+  x = x * sqrt(x);
+  double a = 4.0 * alpha;
+  double num = x * a * a * a * a;
   if (cond1) {
     return sqrt(num / 105.0);
   }
@@ -72,19 +80,28 @@ __device__ double chemtools::normalization_primitive_g(double alpha, int nx, int
 /// Compute the normalization constant of a single primitive Pure (Spherical Harmonics) Gaussian, D-type only.
 __device__ double chemtools::normalization_primitive_pure_d(double alpha) {
   // Angular momentum L is 2 in this case.
-  return sqrt(pow(2.0 * alpha / CUDART_PI_D, 1.5) * pow(4.0 * alpha, 2) / 3.0);
+  double a = 4.0 * alpha;
+  double x = 2.0 * alpha / CUDART_PI_D;
+  x = x * sqrt(x);
+  return sqrt(x * a * a / 3.0);
 }
 
 __device__ double chemtools::normalization_primitive_pure_f(double alpha) {
   // Angular momentum is L is 3 in this case
   // Formula is ((2a / pi)^1.5  *  (4 a)^L  / (2L - 1)!! )^0.5
-  return sqrt(pow(2.0 * alpha / CUDART_PI_D, 1.5) * pow(4.0 * alpha, 3) / 15.0);
+  double a = 4.0 * alpha;
+  double x = 2.0 * alpha / CUDART_PI_D;
+  x = x * sqrt(x);
+  return sqrt(x * a * a * a / 15.0);
 }
 
 __device__ double chemtools::normalization_primitive_pure_g(double alpha) {
   // Angular momentum is L is 4 in this case
   // Formula is ((2a / pi)^1.5  *  (4 a)^L  / (2L - 1)!! )^0.5
-  return sqrt(pow(2.0 * alpha / CUDART_PI_D, 1.5) * pow(4.0 * alpha, 4) / 105.0);
+  double a = 4.0 * alpha;
+  double x = 2.0 * alpha / CUDART_PI_D;
+  x = x * sqrt(x);
+  return sqrt(x * a * a * a * a / 105.0);
 }
 
 /// Compute the Pure/Harmonic basis functions for d-type shells.
@@ -102,13 +119,13 @@ __device__ double chemtools::solid_harmonic_function_d(int m, double r_Ax, doubl
     return sqrt(3.) * r_Ay * r_Az;
   }
   else if (m == 0) {
-    return (2 * pow(r_Az, 2.0) - pow(r_Ax, 2.0) - pow(r_Ay, 2.0)) / 2.0;
+    return (2 * (r_Az * r_Az) - (r_Ax * r_Ax) - (r_Ay * r_Ay)) / 2.0;
   }
   else if (m == 1) {
     return sqrt(3.) * r_Ax * r_Az;
   }
   else if (m == 2) {
-    return sqrt(3.0) * (pow(r_Ax, 2.0) -  pow(r_Ay, 2.0)) / 2.0;
+    return sqrt(3.0) * ((r_Ax * r_Ax) -  (r_Ay * r_Ay)) / 2.0;
   }
   else {
     assert(0);
@@ -127,25 +144,25 @@ __device__ double chemtools::solid_harmonic_function_f(int m, double r_Ax, doubl
   // These are obtained from the table in pg 211 of Helgeker's book.
 
   if (m == -3) {
-    return sqrt(2.5) * (3.0 * pow(r_Ax, 2.0) -  pow(r_Ay, 2.0)) * r_Ay / 2.0;
+    return sqrt(2.5) * (3.0 * (r_Ax * r_Ax) -  (r_Ay * r_Ay)) * r_Ay / 2.0;
   }
   else if (m == -2) {
     return sqrt(15.0) * r_Ax * r_Ay * r_Az;
   }
   else if (m == -1) {
-    return sqrt(1.5) * (4.0 * pow(r_Az, 2.0) - (pow(r_Ax, 2.0) + pow(r_Ay, 2.0))) * r_Ay / 2.0;
+    return sqrt(1.5) * (4.0 * (r_Az * r_Az) - ((r_Ax * r_Ax) + (r_Ay * r_Ay))) * r_Ay / 2.0;
   }
   else if (m == 0) {
-    return (2.0 * pow(r_Az, 2.0) - 3.0 * (pow(r_Ax, 2.0) + pow(r_Ay, 2.0))) * r_Az / 2.0;
+    return (2.0 * (r_Az * r_Az) - 3.0 * ((r_Ax * r_Ax) + (r_Ay * r_Ay))) * r_Az / 2.0;
   }
   else if (m == 1) {
-    return sqrt(1.5) * (4.0 * pow(r_Az, 2.0) - (pow(r_Ax, 2.0) + pow(r_Ay, 2.0))) * r_Ax / 2.0;
+    return sqrt(1.5) * (4.0 * (r_Az * r_Az) - ((r_Ax * r_Ax) + (r_Ay * r_Ay))) * r_Ax / 2.0;
   }
   else if (m == 2) {
-    return sqrt(15.0) * (pow(r_Ax, 2.0) -  pow(r_Ay, 2.0)) * r_Az / 2.0;
+    return sqrt(15.0) * ((r_Ax * r_Ax) -  (r_Ay * r_Ay)) * r_Az / 2.0;
   }
   else if (m == 3) {
-    return sqrt(2.5) * (pow(r_Ax, 2.0) -  3.0 * pow(r_Ay, 2.0)) * r_Ax / 2.0;
+    return sqrt(2.5) * ((r_Ax * r_Ax) -  3.0 * (r_Ay * r_Ay)) * r_Ax / 2.0;
   }
   else {
     assert(0);
@@ -158,43 +175,43 @@ __device__ double chemtools::solid_harmonic_function_f(int m, double r_Ax, doubl
 /// Compute the Pure/Harmonic basis functions for g-type shells.
 __device__ double chemtools::solid_harmonic_function_g(int m, double r_Ax, double r_Ay, double r_Az) {
   if (m == 4) {
-    return sqrt(35.0) * (pow(r_Ax, 4.0) - 6.0 * pow(r_Ax, 2.0) * pow(r_Ay, 2.0) + pow(r_Ay, 4.0)) / 8.0;
+    return sqrt(35.0) * ((r_Ax * r_Ax * r_Ax * r_Ax) - 6.0 * (r_Ax * r_Ax) * (r_Ay * r_Ay) + (r_Ay * r_Ay * r_Ay * r_Ay)) / 8.0;
   }
   else if (m == 3) {
-    return sqrt(35.0 / 2.0) * (pow(r_Ax, 2.0) - 3.0 * pow(r_Ay, 2.0)) * r_Ax * r_Az / 2.0;
+    return sqrt(35.0 / 2.0) * ((r_Ax * r_Ax) - 3.0 * (r_Ay * r_Ay)) * r_Ax * r_Az / 2.0;
   }
   else if (m == 2) {
-    return sqrt(5.0) * (6.0 * pow(r_Az, 2.0) -
-        (pow(r_Ay, 2.0) + pow(r_Ax, 2.0)))
-        * (pow(r_Ax, 2.0) - pow(r_Ay, 2.0)) / 4.0;
+    return sqrt(5.0) * (6.0 * (r_Az * r_Az) -
+        ((r_Ay * r_Ay) + (r_Ax * r_Ax)))
+        * ((r_Ax * r_Ax) - (r_Ay * r_Ay)) / 4.0;
   }
   else if (m == 1) {
-    return sqrt(2.5) * (4.0 * pow(r_Az, 2.0) - 3.0 * pow(r_Ax, 2.0) - 3.0 * pow(r_Ay, 2.0))
+    return sqrt(2.5) * (4.0 * (r_Az * r_Az) - 3.0 * (r_Ax * r_Ax) - 3.0 * (r_Ay * r_Ay))
         * r_Ax * r_Az / 2.0;
   }
   else if (m == 0) {
-    double rad_sq = (pow(r_Az, 2.0) + pow(r_Ay, 2.0) + pow(r_Ax, 2.0));
+    double rad_sq = ((r_Az * r_Az) + (r_Ay * r_Ay) + (r_Ax * r_Ax));
     return (
-        35.0 * pow(r_Az, 4.0) -
-            30.0 * pow(r_Az, 2.0) * rad_sq +
-            3.0 * pow(rad_sq, 2.0)
+        35.0 * (r_Az * r_Az * r_Az * r_Az) -
+            30.0 * (r_Az * r_Az) * rad_sq +
+            3.0 * rad_sq * rad_sq
     ) / 8.0;
   }
   else if (m == -1) {
-    return sqrt(2.5) * (7.0 * pow(r_Az, 2.0) -
-        3.0 *  (pow(r_Az, 2.0) + pow(r_Ay, 2.0) + pow(r_Ax, 2.0)))
+    return sqrt(2.5) * (7.0 * (r_Az * r_Az) -
+        3.0 *  ((r_Az * r_Az) + (r_Ay * r_Ay) + (r_Ax * r_Ax)))
         * r_Ay * r_Az / 2.0;
   }
   else if (m == -2) {
-    return sqrt(5.0) * (7.0 * pow(r_Az, 2.0) -
-        (pow(r_Az, 2.0) + pow(r_Ay, 2.0) + pow(r_Ax, 2.0)))
+    return sqrt(5.0) * (7.0 * (r_Az * r_Az) -
+        ((r_Az * r_Az) + (r_Ay * r_Ay) + (r_Ax * r_Ax)))
         * r_Ax * r_Ay / 2.0;
   }
   else if (m == -3) {
-    return sqrt(35.0 / 2.0) * (3.0 * pow(r_Ax, 2.0) - pow(r_Ay, 2.0)) * r_Ay * r_Az / 2.0;
+    return sqrt(35.0 / 2.0) * (3.0 * (r_Ax * r_Ax) - (r_Ay * r_Ay)) * r_Ay * r_Az / 2.0;
   }
   else if (m == -4) {
-    return sqrt(35.0) * (pow(r_Ax, 2.0) - pow(r_Ay, 2.0)) * r_Ax * r_Ay / 2.0;
+    return sqrt(35.0) * ((r_Ax * r_Ax) - (r_Ay * r_Ay)) * r_Ax * r_Ay / 2.0;
   }
   else {
     assert(0);
