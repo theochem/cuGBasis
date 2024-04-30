bd091d0bdcf09518bfdde20c5b2cd72aebcbc4ad
Ali-Tehrani <alirezatehrani24@gmail.com>
2024-04-30 11:50:21 -0400

    Comment out cubic grid tests as its depcrecitated

--- tests/test_density.py
+++ tests/test_density.py
@@ -26,19 +26,19 @@ def test_electron_density_against_horton(fchk):
     assert np.all(np.abs(cpu_gradient - gpu_gradient) < 1e-8)
 
 
-@pytest.mark.parametrize("fchk",
-                         [
-                             "./tests/data/E948_rwB97XD_def2SVP.fchk",
-                             "./tests/data/h2o.fchk",
-                             "./tests/data/ch4.fchk",
-                             "./tests/data/qm9_000092_HF_cc-pVDZ.fchk"
-                         ]
-                         )
-def test_electron_density_on_cubic_grid_against_horton(fchk):
-    mol = cugbasis.Molecule(fchk)
-    mol2 = Molecule.from_file(fchk)
-
-    grid = UniformGrid.from_molecule(mol2.numbers, mol2.coordinates, spacing=0.5)
-    gpu_density = mol.compute_density_on_cubic_grid(grid.origin, grid.axes, grid.shape, False)
-    cpu_density = mol2.compute_density(grid.points)
-    assert np.all(np.abs(cpu_density - gpu_density) < 1e-8)
+# @pytest.mark.parametrize("fchk",
+#                          [
+#                              "./tests/data/E948_rwB97XD_def2SVP.fchk",
+#                              "./tests/data/h2o.fchk",
+#                              "./tests/data/ch4.fchk",
+#                              "./tests/data/qm9_000092_HF_cc-pVDZ.fchk"
+#                          ]
+#                          )
+# def test_electron_density_on_cubic_grid_against_horton(fchk):
+#     mol = cugbasis.Molecule(fchk)
+#     mol2 = Molecule.from_file(fchk)
+#
+#     grid = UniformGrid.from_molecule(mol2.numbers, mol2.coordinates, spacing=0.5)
+#     gpu_density = mol.compute_density_on_cubic_grid(grid.origin, grid.axes, grid.shape, False)
+#     cpu_density = mol2.compute_density(grid.points)
+#     assert np.all(np.abs(cpu_density - gpu_density) < 1e-8)
