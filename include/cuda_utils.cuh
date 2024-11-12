ddd8102c4effc69fce6dedacd3464a4776baa525
Ali-Tehrani <alirezatehrani24@gmail.com>
2024-11-11 23:24:50 -0500

    Increase by 20 ms for evaluating gradient
    - Rewrote hadamard product for tensor and array

--- include/cuda_utils.cuh
+++ include/cuda_utils.cuh
@@ -7,6 +7,8 @@
 #include "cublas_v2.h"
 
 #define CUDART_PI_D 3.141592653589793238462643383279502884197169
+#define CUDA_CHECK(err) cuda_check_errors(err, __FILE__, __LINE__)
+#define CUBLAS_CHECK(err) cublas_check_errors(err, __FILE__, __LINE__)
 
 namespace chemtools {
 
@@ -17,9 +19,35 @@ __global__ void copy_arrays(double* d_output, double* d_input, int numb_elements
 __global__ void set_identity_row_major(double* d_array, int nrows, int ncols);
 
 /// assumes both matrices are in the same order. The second one has an output.
-__global__ void hadamard_product(double* d_array1, double* d_array2, int numb_row, int numb_col);
+__global__ void hadamard_product(
+    double* __restrict__ d_array1,
+    const double* __restrict__ d_array2,
+    int numb_row,
+    int numb_col
+);
 __global__ void hadamard_product_outplace(double* d_output, double* d_array1, double* d_array2, int numb_row, int numb_col);
 __global__ void hadamard_product_with_vector_along_row_inplace_for_trapezoidal(double* d_array, const double* d_vec, int numb_row, int numb_col);
+/// Hadamard product a tensor (D, M, N) against (M, N) and multiply by two, used by gradient
+template <int D>
+__global__ void hadamard_product_tensor_with_mat_with_multiply_by_two(
+    double* __restrict__ da1,
+    const double* __restrict__ da2,
+    int size_da2)
+{
+    int global_index = blockIdx.x * blockDim.x + threadIdx.x;
+    if(global_index < size_da2) {
+        #pragma unroll D
+        for (int i = 0; i < D; i++) {
+            da1[global_index + i * size_da2] *= 2.0 * da2[global_index];
+        }
+    }
+}
+
+/// Square-root of each element of the array
+__global__ void square_root(double* d_array, int numb_elements);
+
+/// square each element of the array
+__global__ void pow_inplace(double* d_array, double power, int numb_elements);
 
 /// Square-root of each element of the array
 __global__ void square_root(double* d_array, int numb_elements);
@@ -51,46 +79,34 @@ __global__ void print_all(double* arr, int number);
 __global__ void print_firstt_ten_elements(int* arr);
 __global__ void print_matrix(double* arr, int row, int column);
 
+
 /// Checks for cuda errors.
-__host__ inline void cuda_check_errors(cudaError_t error) {
+__host__ inline void cuda_check_errors(cudaError_t error, const char* file, int line) {
   if (error != cudaSuccess) {
-    std::cout << "Cuda Error is " << cudaGetErrorString(error) << std::endl;
+    std::cout << "Cuda Error in " << file << ":" << line << \
+    "is " << cudaGetErrorString(error) << "( " << error << ") \n" << std::endl;
     throw error;
   }
 }
 
+__host__ inline void cuda_check_errors(cudaError_t error) {
+    if (error != cudaSuccess) {
+        std::cout << "CUDA Error is " << cudaGetErrorString(error) << "( " << error << ") \n" << std::endl;
+        throw error;
+    }
+}
 /// Checks for cuBLAS API errors
-__host__ static const char* cublas_check_errors(cublasStatus_t error)
+__host__ static const char* cublas_check_errors(cublasStatus_t error, const char* file, int line)
 {
-  switch (error)
-  {
-    case CUBLAS_STATUS_SUCCESS:
-      return "CUBLAS_STATUS_SUCCESS";
-
-    case CUBLAS_STATUS_NOT_INITIALIZED:
-      return "CUBLAS_STATUS_NOT_INITIALIZED";
-
-    case CUBLAS_STATUS_ALLOC_FAILED:
-      return "CUBLAS_STATUS_ALLOC_FAILED";
-
-    case CUBLAS_STATUS_INVALID_VALUE:
-      return "CUBLAS_STATUS_INVALID_VALUE";
-
-    case CUBLAS_STATUS_ARCH_MISMATCH:
-      return "CUBLAS_STATUS_ARCH_MISMATCH";
-
-    case CUBLAS_STATUS_MAPPING_ERROR:
-      return "CUBLAS_STATUS_MAPPING_ERROR";
-
-    case CUBLAS_STATUS_EXECUTION_FAILED:
-      return "CUBLAS_STATUS_EXECUTION_FAILED";
-
-    case CUBLAS_STATUS_INTERNAL_ERROR:
-      return "CUBLAS_STATUS_INTERNAL_ERROR";
-  }
-  return "unknown error";
+    if (error != CUBLAS_STATUS_SUCCESS)
+        std::cout << "cuBLAS error in " << file << ":" << line << "\n Status code: " << error << "\n";
 }
 
+__host__ static const char* cublas_check_errors(cublasStatus_t error)
+{
+    if (error != CUBLAS_STATUS_SUCCESS)
+        std::cout << "cuBLAS error in " << "\n Status code: " << error << "\n";
+}
 
 }
 #endif //CHEMTOOLS_CUDA_INCLUDE_CUDA_UTILS_CUH_
