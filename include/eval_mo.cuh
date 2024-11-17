ed7e79874e23ce6b6a1be86fdb3528531fcd5a2e
Ali-Tehrani <alirezatehrani24@gmail.com>
2024-11-17 14:14:46 -0500

    Add molecular orbitals and derivatives (not optimized)
    - Add tests

---
+++ include/eval_mo.cuh
@@ -0,0 +1,48 @@
+
+#ifndef CUGBASIS_SRC_EVAL_MO_CUH_
+#define CUGBASIS_SRC_EVAL_MO_CUH_
+
+#include "contracted_shell.h"
+#include "iodata.h"
+
+namespace chemtools {
+
+/**
+ * Evaluate the molecular orbitals from storing molecular basis in constant memory over any grid.
+ *
+ * @param[in] h_points Array in column-major order that stores the `N` three-dimensional points.
+ * @param[in] n_pts Number of points in d_points.
+ * @param[in] nbasisfuncs The number of basis functions.
+ * @return h_mol_orbitals The molecular orbitals (M, N) coloumn-order evaluated on each point.
+ */
+__host__ std::vector<double> eval_MOs(
+    IOData& iodata, const double* h_points, const int n_pts
+);
+
+
+/**
+ * Evaluate the molecular orbitals derivatives.
+ *
+ * @param[in] h_points Array in column-major order that stores the `N` three-dimensional points.
+ * @param[in] n_pts Number of points in d_points.
+ * @param[in] nbasisfuncs The number of basis functions.
+ * @return h__mol_orbitals_deriv The first derivative molecular orbitals order (3, N, M).
+ */
+__host__ std::vector<double> eval_MOs_derivs(
+    IOData& iodata, const double* h_points, const int n_pts
+);
+
+
+/**
+ * Evaluate the molecular orbitals second derivatives.
+ *
+ * @param[in] h_points Array in column-major order that stores the `N` three-dimensional points.
+ * @param[in] n_pts Number of points in d_points.
+ * @param[in] nbasisfuncs The number of basis functions.
+ * @return h_sec_mol_orbitals The second derivatives MO order (6, N, M)
+ */
+__host__ std::vector<double> eval_MOs_second_derivs(
+    IOData& iodata, const double* h_points, const int n_pts
+);
+}
+#endif //CUGBASIS_SRC_EVAL_MO_CUH_
