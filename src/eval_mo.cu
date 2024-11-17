b992d7ceeb5ac5d112019c81bc94a4fb5fef64f4
Ali-Tehrani <alirezatehrani24@gmail.com>
2024-11-17 14:14:46 -0500

    Add molecular orbitals and derivatives (not optimized)
    - Add tests

---
+++ src/eval_mo.cu
@@ -0,0 +1,397 @@
+
+#include "eval_mo.cuh"
+#include "cuda_utils.cuh"
+#include "basis_to_gpu.cuh"
+#include "eval.cuh"
+
+using namespace chemtools;
+
+__host__ std::vector<double> chemtools::eval_MOs(
+    IOData& iodata, const double* h_points, const int n_pts
+) {
+    cublasHandle_t handle;
+    cublasCreate(&handle);
+    
+    const MolecularBasis molbasis = iodata.GetOrbitalBasis();
+    const int            n_basis  = molbasis.numb_basis_functions();
+    
+    // Calculate Optimal Memory Chunks
+    const size_t MAX_PTS_PER_ITER = 64 * 64 * 32  ;
+    auto   chunks     = GpuMemoryPartitioner::compute(
+        n_basis,
+        [](size_t mem, size_t numb_basis){
+          return ((mem / sizeof(double))  - numb_basis * numb_basis) / (2 * numb_basis);
+        },
+        n_pts,
+        MAX_PTS_PER_ITER
+    );
+    
+    // Resulting Molecular orbitals (M, N) col-order
+    std::vector<double> h_MOs_col(n_pts * n_basis);
+    
+    // Iterate through each chunk of the data set.
+    size_t index_to_copy = 0;  
+    size_t i_iter        = 0;
+    while(index_to_copy < n_pts) {
+        // Calculate number of points to do
+        size_t npts_iter    = std::min(
+            n_pts - i_iter * chunks.pts_per_iter,
+            chunks.pts_per_iter
+        );
+        size_t AO_data_size = npts_iter * n_basis * sizeof(double);
+        
+        // Allocate device memory for contractions row-major (M, N)
+        double *d_AOs = nullptr;
+        CUDA_CHECK(cudaMalloc((double **) &d_AOs, AO_data_size));
+        CUDA_CHECK(cudaMemset(d_AOs, 0, AO_data_size));
+        
+        // Allocate points and copy grid points column-order
+        double *d_pts;
+        CUDA_CHECK(cudaMalloc((double **) &d_pts, sizeof(double) * 3 * npts_iter));
+        #pragma unroll
+        for(int coord = 0; coord < 3; coord++) {
+            CUDA_CHECK(cudaMemcpy(
+                &d_pts[coord * npts_iter],
+                &h_points[coord * n_pts + index_to_copy],
+                sizeof(double) * npts_iter,
+                cudaMemcpyHostToDevice)
+            );
+        }
+        
+        // Evaluate AOs.
+        constexpr int THREADS_PER_BLOCK = 128;
+        dim3 threads(THREADS_PER_BLOCK);
+        dim3 blocks((npts_iter + THREADS_PER_BLOCK - 1) / (THREADS_PER_BLOCK));
+        evaluate_scalar_quantity_density(
+            molbasis,
+            false,
+            false,
+            "rho",
+            d_AOs,
+            d_pts,
+            npts_iter,
+            n_basis,
+            threads,
+            blocks
+        );
+        cudaFree(d_pts);
+        
+        // Allocate device memory for the one_rdm. Number AO = Number MO
+        double *d_one_rdm;
+        CUDA_CHECK(cudaMalloc((double **) &d_one_rdm, n_basis * n_basis * sizeof(double)));
+        CUBLAS_CHECK(cublasSetMatrix(
+            iodata.GetOneRdmShape(),
+            iodata.GetOneRdmShape(),
+            sizeof(double),
+            iodata.GetMOOneRDM(),
+            iodata.GetOneRdmShape(),
+            d_one_rdm,
+            iodata.GetOneRdmShape()
+        ));
+        
+        // Molecular Orbitals (M, N) row
+        double *d_MOs;
+        CUDA_CHECK(cudaMalloc((double **) &d_MOs, AO_data_size));
+        
+        // Matrix mult. of one rdm with the contractions array. Everything is in row major order.
+        double alpha = 1.0, beta = 0.0;
+        CUBLAS_CHECK(cublasDgemm(
+            handle, CUBLAS_OP_N, CUBLAS_OP_N,
+            npts_iter, iodata.GetOneRdmShape(), iodata.GetOneRdmShape(),
+            &alpha,
+            d_AOs, npts_iter,
+            d_one_rdm, iodata.GetOneRdmShape(),
+            &beta,
+            d_MOs, npts_iter
+        ));
+        cudaFree(d_one_rdm);
+        
+        // Transfer electron density from device memory to host memory.
+        //    Since I'm computing a sub-grid at a time, need to update the index h_electron_density, accordingly.
+        //    Note that d_mol_orbs is in row-major order with shape (M, N)
+        std::vector<double> h_mol_orbitals_row(n_basis * npts_iter);
+        cuda_check_errors(cudaMemcpy(h_mol_orbitals_row.data(),
+                                     d_MOs,
+                                     sizeof(double) * n_basis * npts_iter, cudaMemcpyDeviceToHost));
+        cudaFree(d_MOs);
+        
+        // Transfer from row to column major order;
+        for(int i_row = 0; i_row < n_basis; i_row++){
+            for(int j_col = 0; j_col < npts_iter; j_col++) {
+                h_MOs_col[n_basis * index_to_copy + (j_col * n_basis + i_row)] = h_mol_orbitals_row[i_row * npts_iter + j_col];
+            }
+        }
+        
+        // Update lower-bound of the grid for the next iteration
+        index_to_copy += npts_iter;
+        i_iter += 1;  // Update the index for each iteration.
+    }
+    cublasDestroy(handle);
+    
+    return h_MOs_col;
+}
+
+
+__host__ std::vector<double> chemtools::eval_MOs_derivs(
+          IOData &iodata,
+    const double *h_points,
+    const int    n_pts)
+{
+    cublasHandle_t handle;
+    cublasCreate(&handle);
+    
+    const MolecularBasis molbasis = iodata.GetOrbitalBasis();
+    const int            n_basis  = molbasis.numb_basis_functions();
+    
+    // Calculate Optimal Memory Chunks
+    const size_t MAX_PTS_PER_ITER = 64 * 64 * 32  ;
+    auto   chunks     = GpuMemoryPartitioner::compute(
+        n_basis,
+        [](size_t mem, size_t numb_basis){
+          return ((mem / sizeof(double))  - numb_basis * numb_basis) / (4 * numb_basis);
+        },
+        n_pts,
+        MAX_PTS_PER_ITER
+    );
+    
+    // Resulting Molecular orbitals (3, N, M) col-order
+    std::vector<double> h_MOs_col(3 * n_pts * n_basis);
+    
+    // Iterate through each chunk of the data set.
+    size_t index_to_copy = 0;
+    size_t i_iter        = 0;
+    while(index_to_copy < n_pts) {
+        size_t npts_iter = std::min(
+            n_pts - i_iter * chunks.pts_per_iter,
+            chunks.pts_per_iter
+        );
+        size_t AO_deriv_size = sizeof(double) * 3 * npts_iter * n_basis;
+        
+        // Allocate device memory for contractions row-major (M, N)
+        double *d_AOs_derivs = nullptr;
+        CUDA_CHECK(cudaMalloc((double **) &d_AOs_derivs, AO_deriv_size));
+        CUDA_CHECK(cudaMemset(d_AOs_derivs, 0, AO_deriv_size));
+        
+        // Allocate points and copy grid points column-order
+        double *d_pts;
+        CUDA_CHECK(cudaMalloc((double **) &d_pts, sizeof(double) * 3 * npts_iter));
+        #pragma unroll
+        for(int coord = 0; coord < 3; coord++) {
+            CUDA_CHECK(cudaMemcpy(
+                &d_pts[coord * npts_iter],
+                &h_points[coord * n_pts + index_to_copy],
+                sizeof(double) * npts_iter,
+                cudaMemcpyHostToDevice)
+            );
+        }
+        
+        // Evaluate Derivatives of AOs.
+        constexpr int THREADS_PER_BLOCK = 128;
+        dim3 threads(THREADS_PER_BLOCK);
+        dim3 blocks((npts_iter + THREADS_PER_BLOCK - 1) / (THREADS_PER_BLOCK));
+        evaluate_scalar_quantity_density(
+            molbasis,
+            false,
+            false,
+            "rho_deriv",
+            d_AOs_derivs,
+            d_pts,
+            npts_iter,
+            n_basis,
+            threads,
+            blocks
+        );
+        cudaFree(d_pts);
+        
+        // Allocate device memory for the one_rdm. Number AO = Number MO
+        double *d_one_rdm;
+        CUDA_CHECK(cudaMalloc((double **) &d_one_rdm, n_basis * n_basis * sizeof(double)));
+        CUBLAS_CHECK(cublasSetMatrix(
+            iodata.GetOneRdmShape(),
+            iodata.GetOneRdmShape(),
+            sizeof(double),
+            iodata.GetMOOneRDM(),
+            iodata.GetOneRdmShape(),
+            d_one_rdm,
+            iodata.GetOneRdmShape()
+        ));
+        
+        // Resulting ith MO Deriv
+        double *d_MOs_derivi;
+        CUDA_CHECK(cudaMalloc((double **) &d_MOs_derivi, sizeof(double) * npts_iter * n_basis));
+        
+        // For each derivative, calculate the derivative of MOs.
+        double alpha = 1.0, beta = 0.0;
+        #pragma unroll 3
+        for (int i_deriv = 0; i_deriv < 3; i_deriv++) {
+            // Ith deriv of AOs (M, N) row-order
+            double *d_ith_deriv = &d_AOs_derivs[i_deriv * npts_iter * n_basis];
+            
+            // Matrix multiple one-rdm with the ith derivative of contractions
+            CUBLAS_CHECK(cublasDgemm(
+                handle, CUBLAS_OP_N, CUBLAS_OP_N,
+                npts_iter, n_basis, n_basis,
+                &alpha,
+                d_ith_deriv, npts_iter,
+                d_one_rdm, n_basis,
+                &beta,
+                d_MOs_derivi, npts_iter
+            ));
+            
+            // Transpose and over-write d_AOs_derivs so its (M, N) row-major
+            array_transpose(
+                handle, &d_AOs_derivs[i_deriv * npts_iter * n_basis],
+                d_MOs_derivi, n_basis, npts_iter
+                );
+            
+            // Copy it over
+            cuda_check_errors(cudaMemcpy(&h_MOs_col[i_deriv * n_basis * n_pts +   n_basis * index_to_copy],
+                                         d_ith_deriv,
+                                         sizeof(double) * n_basis * npts_iter, cudaMemcpyDeviceToHost));
+        }
+        cudaFree(d_MOs_derivi);
+        cudaFree(d_one_rdm);
+        cudaFree(d_AOs_derivs);
+        
+        // Update lower-bound of the grid for the next iteration
+        index_to_copy += npts_iter;
+        i_iter += 1;  // Update the index for each iteration.
+    }
+    cublasDestroy(handle);
+    
+    return h_MOs_col;
+}
+
+
+__host__ std::vector<double> chemtools::eval_MOs_second_derivs(
+    IOData& iodata, const double* h_points, const int n_pts
+)
+{
+    cublasHandle_t handle;
+    cublasCreate(&handle);
+    
+    const MolecularBasis molbasis = iodata.GetOrbitalBasis();
+    const int            n_basis  = molbasis.numb_basis_functions();
+    
+    // Calculate Optimal Memory Chunks
+    const size_t MAX_PTS_PER_ITER = 64 * 64 * 32  ;
+    auto   chunks     = GpuMemoryPartitioner::compute(
+        n_basis,
+        [](size_t mem, size_t numb_basis){
+          return ((mem / sizeof(double))  - numb_basis * numb_basis) / (4 * numb_basis);
+        },
+        n_pts,
+        MAX_PTS_PER_ITER
+    );
+    
+    // Resulting Molecular orbitals (6, N, M) col-order
+    std::vector<double> h_MOs_sec_col(6 * n_pts * n_basis);
+    
+    // Iterate through each chunk of the data set.
+    size_t index_to_copy = 0;
+    size_t i_iter        = 0;
+    while(index_to_copy < n_pts) {
+        size_t npts_iter = std::min(
+            n_pts - i_iter * chunks.pts_per_iter,
+            chunks.pts_per_iter
+        );
+        size_t AO_deriv_size = sizeof(double) * 6 * npts_iter * n_basis;
+        
+        // Allocate device memory for contractions row-major (M, N)
+        double *d_AOs_sec_derivs = nullptr;
+        CUDA_CHECK(cudaMalloc((double **) &d_AOs_sec_derivs, AO_deriv_size));
+        CUDA_CHECK(cudaMemset(d_AOs_sec_derivs, 0, AO_deriv_size));
+        
+        // Allocate points and copy grid points column-order
+        double *d_pts;
+        CUDA_CHECK(cudaMalloc((double **) &d_pts, sizeof(double) * 3 * npts_iter));
+        #pragma unroll
+        for(int coord = 0; coord < 3; coord++) {
+            CUDA_CHECK(cudaMemcpy(
+                &d_pts[coord * npts_iter],
+                &h_points[coord * n_pts + index_to_copy],
+                sizeof(double) * npts_iter,
+                cudaMemcpyHostToDevice)
+            );
+        }
+        
+        // Evaluate Derivatives of AOs.
+        constexpr int THREADS_PER_BLOCK = 128;
+        dim3 threads(THREADS_PER_BLOCK);
+        dim3 blocks((npts_iter + THREADS_PER_BLOCK - 1) / (THREADS_PER_BLOCK));
+        evaluate_scalar_quantity_density(
+            molbasis,
+            false,
+            false,
+            "rho_hess",
+            d_AOs_sec_derivs,
+            d_pts,
+            npts_iter,
+            n_basis,
+            threads,
+            blocks
+        );
+        cudaFree(d_pts);
+        
+        // Allocate device memory for the one_rdm. Number AO = Number MO
+        double *d_one_rdm;
+        CUDA_CHECK(cudaMalloc((double **) &d_one_rdm, n_basis * n_basis * sizeof(double)));
+        CUBLAS_CHECK(cublasSetMatrix(
+            iodata.GetOneRdmShape(),
+            iodata.GetOneRdmShape(),
+            sizeof(double),
+            iodata.GetMOOneRDM(),
+            iodata.GetOneRdmShape(),
+            d_one_rdm,
+            iodata.GetOneRdmShape()
+        ));
+        
+        // Resulting ith MO Deriv
+        double *d_MOs_sec_derivi;
+        CUDA_CHECK(cudaMalloc((double **) &d_MOs_sec_derivi, sizeof(double) * npts_iter * n_basis));
+        
+        // From atomic orbitals calculate MO second- derivatives
+        double alpha = 1.0;
+        double beta = 0.0;
+        int i_sec_deriv = 0;
+        for(int i_deriv = 0; i_deriv < 3; i_deriv++) {
+            for (int j_deriv = i_deriv; j_deriv < 3; j_deriv++) {
+                // Get the second derivative: `i_deriv`th and `j_deriv`th derivative.
+                double *d_sec_deriv = &d_AOs_sec_derivs[i_sec_deriv * npts_iter * n_basis];
+                
+                // Matrix multiple one-rdm with the ith derivative of contractions
+                CUBLAS_CHECK(cublasDgemm(
+                    handle, CUBLAS_OP_N, CUBLAS_OP_N,
+                    npts_iter, n_basis, n_basis,
+                    &alpha,
+                    d_sec_deriv, npts_iter,
+                    d_one_rdm, n_basis,
+                    &beta,
+                    d_MOs_sec_derivi, npts_iter
+                ));
+                
+                // Transpose and over-write d_AOs_derivs so its (M, N) row-major
+                array_transpose(
+                    handle, &d_AOs_sec_derivs[i_sec_deriv * npts_iter * n_basis],
+                    d_MOs_sec_derivi, n_basis, npts_iter
+                );
+                
+                // Copy it over
+                cuda_check_errors(cudaMemcpy(&h_MOs_sec_col[i_sec_deriv * n_basis * n_pts +  n_basis * index_to_copy],
+                                             d_sec_deriv,
+                                             sizeof(double) * n_basis * npts_iter, cudaMemcpyDeviceToHost));
+                i_sec_deriv++;
+            }
+        }
+        cudaFree(d_MOs_sec_derivi);
+        cudaFree(d_one_rdm);
+        cudaFree(d_AOs_sec_derivs);
+    
+        // Update lower-bound of the grid for the next iteration
+        index_to_copy += npts_iter;
+        i_iter += 1;  // Update the index for each iteration.
+    }
+    cublasDestroy(handle);
+    
+    return h_MOs_sec_col;
+}
--- tests/test_eval_mo.cu
+++ tests/test_eval_mo.cu
@@ -35,18 +35,13 @@ TEST_CASE( "Test Molecular Orbitals Against gbasis on random grid", "[evaluate_m
         "./tests/data/ch4.fchk",
         "./tests/data/qm9_000092_HF_cc-pVDZ.fchk",
         "./tests/data/qm9_000104_PBE1PBE_pcS-3.fchk",
-        "./tests/data/DUTLAF10_0_q000_m01_k00_force_uwb97xd_def2svpd.fchk",
-        "./tests/data/qm9_000092_HF_cc-pVDZ.wfx",
-        "./tests/data/DASXIE_0_q000_m01_k00_force_uwb97xd_def2svpd.wfn",
-        "./tests/data/DASXIE_0_q000_m01_k00_force_uwb97xd_def2svpd.wfx",
-        "./tests/data/PHE_TRP_0_q000_m01_k00_force_uwb97xd_def2svpd.wfn",
-        "./tests/data/PHE_TRP_0_q000_m01_k00_force_uwb97xd_def2svpd.wfx"
+        "./tests/data/DUTLAF10_0_q000_m01_k00_force_uwb97xd_def2svpd.fchk"
     );
     printf("Test: %s \n", fchk_file.c_str());
     chemtools::IOData iodata = chemtools::get_molecular_basis_from_fchk(fchk_file);
 
     // Gemerate random grid.
-    int numb_pts = 1000;
+    int numb_pts = 500'000;
     std::vector<double> points(3 * numb_pts);
     std::random_device rnd_device;
     std::mt19937  merseene_engine {rnd_device()};
@@ -56,7 +51,7 @@ TEST_CASE( "Test Molecular Orbitals Against gbasis on random grid", "[evaluate_m
 
     // Evaluate electron density on the cube
     printf("Evaluate Molecular Orbitals \n");
-    std::vector<double> result = chemtools::evaluate_molecular_orbitals_on_any_grid(iodata, points.data(), numb_pts);
+    std::vector<double> result = chemtools::eval_MOs(iodata, points.data(), numb_pts);
 
     //Transfer result to pybind11 without copying
     pybind11::array_t<double, pybind11::array::c_style | pybind11::array::forcecast> py_result =
@@ -85,7 +80,7 @@ rdm = (coeffs * iodata.mo.occs).dot(coeffs.T)
 points = points.reshape((numb_pts, 3), order="F")
 points = np.array(points, dtype=np.float64)
 
-random_indices = np.unique(np.random.randint(0, len(points), 50000))
+random_indices = np.unique(np.random.randint(0, len(points), 10000))
 points = points[random_indices, :]
 
 mol_orbitals = rdm.dot(evaluate_basis(basis, points))
@@ -99,4 +94,181 @@ assert result, "Molecular orbitals on GPU doesn't match gbasis."
       REQUIRE(true);
     }
   } // Need this so that the python object doesn't outline the interpretor when we close it up.
+}
+
+
+
+TEST_CASE( "Test Molecular Orbitals Derivatives Against gbasis on random grid", "[evaluate_mol_orbitals_deriv_on_any_grid]" ) {
+    {  // Need this so that the python object doesn't outline the interpretor.
+        // Evaluate the electron density of this example.
+        std::string fchk_file = GENERATE(
+            "./tests/data/atom_01_H_N01_M2_ub3lyp_ccpvtz_g09.fchk",
+            "./tests/data/atom_he.fchk",
+            "./tests/data/atom_be.fchk",
+            "./tests/data/atom_be_f_pure_orbital.fchk",
+            "./tests/data/atom_be_f_cartesian_orbital.fchk",
+            "./tests/data/atom_kr.fchk",
+            "./tests/data/atom_o.fchk",
+            "./tests/data/atom_c_g_pure_orbital.fchk",
+            "./tests/data/atom_mg.fchk",
+            "./tests/data/E948_rwB97XD_def2SVP.fchk",
+            "./tests/data/test.fchk",
+            "./tests/data/test2.fchk",
+            "./tests/data/atom_08_O_N08_M3_ub3lyp_ccpvtz_g09.fchk",
+            "./tests/data/atom_08_O_N09_M2_ub3lyp_ccpvtz_g09.fchk",
+            "./tests/data/h2o.fchk",
+            "./tests/data/ch4.fchk",
+            "./tests/data/qm9_000092_HF_cc-pVDZ.fchk",
+            "./tests/data/qm9_000104_PBE1PBE_pcS-3.fchk",
+            "./tests/data/DUTLAF10_0_q000_m01_k00_force_uwb97xd_def2svpd.fchk"
+        );
+        printf("Test: %s \n", fchk_file.c_str());
+        chemtools::IOData iodata = chemtools::get_molecular_basis_from_fchk(fchk_file);
+        
+        // Gemerate random grid.
+        int numb_pts = 1000;
+        std::vector<double> points(3 * numb_pts);
+        std::random_device rnd_device;
+        std::mt19937  merseene_engine {rnd_device()};
+        std::uniform_real_distribution<double> dist {-5, 5};
+        auto gen = [&dist, &merseene_engine](){return dist(merseene_engine);};
+        std::generate(points.begin(), points.end(), gen);
+        
+        // Evaluate electron density on the cube
+        printf("Evaluate First Derivative Molecular Orbitals \n");
+        std::vector<double> result = chemtools::eval_MOs_derivs(iodata, points.data(), numb_pts);
+        
+        //Transfer result to pybind11 without copying
+        pybind11::array_t<double, pybind11::array::c_style | pybind11::array::forcecast> py_result =
+                                      chemtools::as_pyarray_from_vector(result);
+        pybind11::array_t<double, pybind11::array::c_style | pybind11::array::forcecast>
+            py_points = chemtools::as_pyarray_from_vector(points);
+        
+        auto locals = py::dict(
+            "true_result"_a=py_result, "fchk_path"_a=fchk_file, "points"_a = py_points, "numb_pts"_a = numb_pts,
+            "nbasis"_a = iodata.GetOneRdmShape()
+        );
+        py::exec(R"(
+import numpy as np
+from gbasis.evals.eval_deriv import evaluate_deriv_basis
+from iodata import load_one
+from gbasis.wrappers import from_iodata
+from iodata.basis import convert_conventions, HORTON2_CONVENTIONS
+
+true_result = true_result.reshape((3, numb_pts, nbasis), order="C")
+
+iodata = load_one(fchk_path)
+basis = from_iodata(iodata)
+coeffs = iodata.mo.coeffs
+rdm = (coeffs * iodata.mo.occs).dot(coeffs.T)
+
+points = points.reshape((numb_pts, 3), order="F")
+points = np.array(points, dtype=np.float64)
+
+random_indices = np.unique(np.random.randint(0, len(points), 1000))
+points = points[random_indices, :]
+
+for i, ord in enumerate([[1, 0, 0], [0, 1, 0], [0, 0, 1]]):
+    ord = np.array(ord)
+    mat = evaluate_deriv_basis(basis, points, ord)
+    mol_orbitals = rdm.dot(mat)
+
+    err = np.abs(mol_orbitals - true_result[i, random_indices, :].T)
+    result = np.all(err < 1e-8)
+    print(f"Max Error {np.max(err)}     Mean Err {np.mean(err)}    Std Err {np.std(err)}")
+    assert result, "Molecular orbitals on GPU doesn't match gbasis."
+    )", py::globals(), locals);
+        
+        if (!locals["result"].cast<bool>()) {
+            REQUIRE(true);
+        }
+    } // Need this so that the python object doesn't outline the interpretor when we close it up.
+}
+
+
+TEST_CASE( "Test Molecular Orbitals Sec Derivatives Against gbasis on random grid", "[evaluate_mol_orbitals_sec_deriv_on_any_grid]" ) {
+    {  // Need this so that the python object doesn't outline the interpretor.
+        // Evaluate the electron density of this example.
+        std::string fchk_file = GENERATE(
+            "./tests/data/atom_01_H_N01_M2_ub3lyp_ccpvtz_g09.fchk",
+            "./tests/data/atom_he.fchk",
+            "./tests/data/atom_be.fchk",
+            "./tests/data/atom_be_f_pure_orbital.fchk",
+            "./tests/data/atom_be_f_cartesian_orbital.fchk",
+            "./tests/data/atom_kr.fchk",
+            "./tests/data/atom_o.fchk",
+            "./tests/data/atom_c_g_pure_orbital.fchk",
+            "./tests/data/atom_mg.fchk",
+            "./tests/data/E948_rwB97XD_def2SVP.fchk",
+            "./tests/data/test.fchk",
+            "./tests/data/test2.fchk",
+            "./tests/data/atom_08_O_N08_M3_ub3lyp_ccpvtz_g09.fchk",
+            "./tests/data/atom_08_O_N09_M2_ub3lyp_ccpvtz_g09.fchk",
+            "./tests/data/h2o.fchk",
+            "./tests/data/ch4.fchk",
+            "./tests/data/qm9_000092_HF_cc-pVDZ.fchk",
+            "./tests/data/qm9_000104_PBE1PBE_pcS-3.fchk",
+            "./tests/data/DUTLAF10_0_q000_m01_k00_force_uwb97xd_def2svpd.fchk"
+        );
+        printf("Test: %s \n", fchk_file.c_str());
+        chemtools::IOData iodata = chemtools::get_molecular_basis_from_fchk(fchk_file);
+        
+        // Gemerate random grid.
+        int numb_pts = 1000;
+        std::vector<double> points(3 * numb_pts);
+        std::random_device rnd_device;
+        std::mt19937  merseene_engine {rnd_device()};
+        std::uniform_real_distribution<double> dist {-5, 5};
+        auto gen = [&dist, &merseene_engine](){return dist(merseene_engine);};
+        std::generate(points.begin(), points.end(), gen);
+        
+        // Evaluate electron density on the cube
+        printf("Evaluate Second Derivatives Molecular Orbitals \n");
+        std::vector<double> result = chemtools::eval_MOs_second_derivs(iodata, points.data(), numb_pts);
+        
+        //Transfer result to pybind11 without copying
+        pybind11::array_t<double, pybind11::array::c_style | pybind11::array::forcecast> py_result =
+                                      chemtools::as_pyarray_from_vector(result);
+        pybind11::array_t<double, pybind11::array::c_style | pybind11::array::forcecast>
+            py_points = chemtools::as_pyarray_from_vector(points);
+        
+        auto locals = py::dict(
+            "true_result"_a=py_result, "fchk_path"_a=fchk_file, "points"_a = py_points, "numb_pts"_a = numb_pts,
+            "nbasis"_a = iodata.GetOneRdmShape()
+        );
+        py::exec(R"(
+import numpy as np
+from gbasis.evals.eval_deriv import evaluate_deriv_basis
+from iodata import load_one
+from gbasis.wrappers import from_iodata
+from iodata.basis import convert_conventions, HORTON2_CONVENTIONS
+
+true_result = true_result.reshape((6, numb_pts, nbasis), order="C")
+
+iodata = load_one(fchk_path)
+basis = from_iodata(iodata)
+coeffs = iodata.mo.coeffs
+rdm = (coeffs * iodata.mo.occs).dot(coeffs.T)
+
+points = points.reshape((numb_pts, 3), order="F")
+points = np.array(points, dtype=np.float64)
+
+random_indices = np.unique(np.random.randint(0, len(points), 1000))
+points = points[random_indices, :]
+
+for i, ord in enumerate([[2, 0, 0], [1, 1, 0], [1, 0, 1], [0, 2, 0], [0, 1, 1], [0, 0, 2]]):
+    ord = np.array(ord)
+    mat = evaluate_deriv_basis(basis, points, ord)
+    mol_orbitals = rdm.dot(mat)
+
+    err = np.abs(mol_orbitals - true_result[i, random_indices, :].T)
+    result = np.all(err < 1e-8)
+    print(f"Max Error {np.max(err)}     Mean Err {np.mean(err)}    Std Err {np.std(err)}")
+    assert result, "Molecular orbitals on GPU doesn't match gbasis."
+    )", py::globals(), locals);
+        
+        if (!locals["result"].cast<bool>()) {
+            REQUIRE(true);
+        }
+    } // Need this so that the python object doesn't outline the interpretor when we close it up.
 }
