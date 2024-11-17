07af02e84eea3e9032f3feb570b88db78c1f2b46
Ali-Tehrani <alirezatehrani24@gmail.com>
2024-11-17 09:35:51 -0500

    Refactor, change imports and remove unused methods

--- include/contracted_shell.h
+++ include/contracted_shell.h
@@ -7,74 +7,78 @@
 
 namespace chemtools {
 
-inline int get_number_of_basis_functions_in_shell(int angmom) {
-  if (angmom == 0) {
-    return 1;
-  }
-  else if (angmom == 1) {
-    return 3;
-  }
-  else if (angmom == 2) {
-    return 6;
-  }
-  else if (angmom == -2) {
-    // (-2) means it is pure type D-type shell
-    return 5;
-  }
-  else if (angmom == 3) {
-    return 10;
-  }
-  else if (angmom == -3) {
-    // (-3) means it is pure type F-type shell
-    return 7;
-  }
-  else if (angmom == 4) {
-    return 15;
-  }
-  else if (angmom == -4) {
-    return 9;
-  }
+/**
+ * @brief Calculates the number of basis functions in a shell based on angular momentum
+ * @param angmom Angular momentum value (positive for Cartesian, negative for pure/spherical)
+ * @return Number of basis functions
+ */
+constexpr int get_number_of_basis_functions_in_shell(int angmom) {
+    switch (std::abs(angmom)) {
+        case 0: return 1;  // S-type
+        case 1: return 3;  // P-type
+        case 2: return (angmom < 0) ? 5 : 6;   // D-type (pure/Cartesian)
+        case 3: return (angmom < 0) ? 7 : 10;  // F-type (pure/Cartesian)
+        case 4: return (angmom < 0) ? 9 : 15;  // G-type (pure/Cartesian)
+        default:
+            assert(false && "Unsupported angular momentum");
+            return 0;
+    }
 }
 
+
+/**
+ * @brief Represents a Generalized Contraction Shell
+ */
 struct GeneralizedContractionShell {
-  std::vector<int>                 angmoms;     // Angular momentum are either 0, 1, 2, -2, 3, -3 (Gaussian FHCK)
+  std::vector<int>                 angmoms;
   std::array<double, 3>            coordinate;
   std::vector<double>              exponents;
   std::vector<std::vector<double>> coefficients;
 };
 
+
+/**
+ * @brief Represents a molecular basis as a collection of contraction shells
+ *        Similar to GBasis
+ */
 class MolecularBasis {
- public:
-  std::vector<GeneralizedContractionShell> shells;
-
-  MolecularBasis() {
-    shells = {GeneralizedContractionShell{{0}, {0, 0, 0}, {0}, {{0}}}};
-  }
-  MolecularBasis(std::vector<GeneralizedContractionShell> shells) {this->shells = shells;}
-  // Number of contracted shells.
-  int numb_contracted_shells(bool decontracted = false) const {
-    if (decontracted) {
-       int counter = 0;
-       for(GeneralizedContractionShell contr_shell : shells) {
-         counter += contr_shell.angmoms.size();
-       }
-       return counter;
+public:
+    // Constructors
+    MolecularBasis()
+        : shells{GeneralizedContractionShell{{0}, {0.0, 0.0, 0.0}, {0.0}, {{0.0}}}} {}
+    explicit MolecularBasis(std::vector<GeneralizedContractionShell> shells_)
+        : shells(std::move(shells_)) {}
+    MolecularBasis(const MolecularBasis&) = default;
+    MolecularBasis& operator=(const MolecularBasis&) = default;
+    MolecularBasis(MolecularBasis&&) noexcept = default;
+    MolecularBasis& operator=(MolecularBasis&&) noexcept = default;
+    
+    std::vector<GeneralizedContractionShell> shells;
+    
+    
+    // Number of contracted shells.
+    int numb_contracted_shells(bool decontracted = false) const {
+        if (decontracted) {
+           int counter = 0;
+           for(GeneralizedContractionShell contr_shell : shells) {
+             counter += contr_shell.angmoms.size();
+           }
+           return counter;
+        }
+        return shells.size();
+    };
+    
+    // Number of contractions/atomic orbitals.
+    int numb_basis_functions() const {
+        int numb = 0;
+        for(GeneralizedContractionShell contr_shell : shells) {
+          for(int angmom : contr_shell.angmoms) {
+            numb += get_number_of_basis_functions_in_shell(angmom);
+          }
+        }
+        return numb;
     }
-    return shells.size();
-  };
-  MolecularBasis(const MolecularBasis& copy) : shells(copy.shells) {};
-
-  // Number of contractions/atomic orbitals.
-  int numb_basis_functions() const {
-    int numb = 0;
-    for(GeneralizedContractionShell contr_shell : shells) {
-      for(int angmom : contr_shell.angmoms) {
-        numb += get_number_of_basis_functions_in_shell(angmom);
-      }
-    }
-    return numb;
-  }
 };
-} // end CHEMTOOLS
+} // namespace chemtools
 
 #endif //CHEMTOOLS_CUDA_CONTRACTED_SHELL_H
